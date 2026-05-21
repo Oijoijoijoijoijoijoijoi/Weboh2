6c7d04cc83ca558fdd473e25767655dfcb3c5894
Anttoni S <anttoni_s@hotmail.com>
2026-05-21 10:23:34 +0300

    chore: fix something, break something?

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ app.use(pinoHttp({logger, autoLogging:{ignore:(r)=>r.url.startsWith("/uploads")}
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "..", "public")));
-app.use("/api/auth", authRouter);
-app.use("/api/questions", questionsRouter);
+app.use("api/auth", authRouter);
+app.use("api/questions", questionsRouter);
 app.use((req, res) => {throw new NotFoundError()});
 app.use(errorHandler);
 
